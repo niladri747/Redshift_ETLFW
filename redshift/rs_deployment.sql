80a97d5979062caf45e4b9cb55ac1dec9919faec
niladri747 <niladri.bhatt@gmail.com>
2022-03-04 10:27:44 +0800

    Update rs_deployment.sql

--- redshift/rs_deployment.sql
+++ redshift/rs_deployment.sql
@@ -6,27 +6,27 @@
  last_load_businessdate: Stores max of last modified record timestamp
  */
 drop table if exists $$schema.etlfw_load_businessdate;
-create table $$schema.load_businessdate
+create table $$schema.etlfw_load_businessdate
 (
 src_tbl varchar(200),
 load_key int,
 last_load_businessdate timestamp
 );
 /*please update the below statement with appropriate values from your environment*/
-insert into db_etlfw.load_businessdate values ('fed_postgres_tpc_ds.web_sales_test_load', 1, '2002-12-31');
-insert into db_etlfw.load_businessdate values ('fed_postgres_tpc_ds.date_dim', 1, '1900-01-01');
+insert into $$schema.etlfw_load_businessdate values ('fed_postgres_tpc_ds.web_sales_test_load', 1, '2002-12-31');
+insert into $$schema.etlfw_load_businessdate values ('fed_postgres_tpc_ds.date_dim', 1, '1900-01-01');
 
 
 /*
- Table :- etlfw_load_businessdate
- Description:- Stores max of last modified record timestamp
- Timestamp for this batch. Used to timestamp the log
+ Table :- etlfw_rs_load_details
+ Description:- table where actions are logged
+ batch_timeT:- imestamp for this batch. Used to timestamp the log
  source_table:- Postgres table to be extracted. Format <schema_name>.<table_name> Schema name should be the redshift external schema
  target_table:- target redshift table. Format <schema_name>.<table_name>
  sync_column:- timestamp field in the src_tbl that records time of data modification
  sync_status:- status for the job
  sync_queies:- Query id of the ETL job
- row_count:-  Total no of rows inserted.deleted.ipdated
+ row_count:-  Total no of rows inserted.deleted.updated
  :-
  */
 drop table if exists $$schema.etlfw_rs_load_details;
